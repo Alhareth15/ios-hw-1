6fca844fce0ccee02f7fd72ad4bc4ea5686edc9e
Alhareth15 <100311546+Alhareth15@users.noreply.github.com>
2022-03-11 21:49:28 +0300

    done

--- Homework 1/Homework 1/ContentView.swift
+++ Homework 1/Homework 1/ContentView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("myFavmovies")
+                .padding()
+            HStack{
+                Image("arrow")
+                    .resizable()
+                    .frame(width: 120, height: 130)
+                Text("arrow")
+                Spacer()
+            }.background(.blue)
+            HStack{
+                Image("gotham")
+                    .resizable()
+                    .frame(width: 120, height: 130)
+                Text("gotham")
+                Spacer()
+            }.background(.green)
+            HStack{
+                Image("lucifer")
+                    .resizable()
+                    .frame(width: 120, height: 130)
+                Text("lucifer")
+                Spacer()
+            }.background(.red)
+            HStack{
+                Image("prison break")
+                    .resizable()
+                    .frame(width: 120, height: 130)
+                Text("prison break")
+                    Spacer()
+                
+            }.background(.gray)
+            HStack{
+                Image("ragnarok")
+                    .resizable()
+                    .frame(width: 120, height: 130)
+                Text("ragnarok")
+                Spacer()
+                    
+            }.background(.yellow)
+        }
+        
     }
 }
 
